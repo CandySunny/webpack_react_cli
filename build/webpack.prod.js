8060a367010478b1c97ec1ba185642144209fa2c
sunmeining <sunmeining@jemcocloud.com>
2020-11-03 11:15:28 +0800

    增加clean-webpack-plugin

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -3,11 +3,15 @@
 const path = require('path')
 const webpackCommonConfig = require('./webpack.common.js')
 const { merge } = require('webpack-merge')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = merge(webpackCommonConfig, {
     mode: 'production',
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: '[name].[contenthash:8].js'
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 })
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css'
 console.log('hello')
 const fn = () => {
     console.log('this is array function')
+    console.log('hello')
 }
